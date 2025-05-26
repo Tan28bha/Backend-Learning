84e911e78710afc5902a5b73bcb35c9407754c72
Tanmay Bhadauria <145855521+Tan28bha@users.noreply.github.com>
2025-05-26 14:24:55 +0530

    Level 1 and 2 completed

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import http from "http"
 const port = 8001
 const server=http.createServer((req,res)=>{
-    res.end("Hello this is my first server and my name is tanmay bhadauria")
+   if(req.url == "/"){
+    res.end("Welcome to home route")
+   }
+   else if(req.url == "/about"){
+    res.end("Welcome to about route")
+   }
+   else if(req.url == "/contact"){
+    res.end("Welcome to contact route")
+   }
+   else{
+    res.end("404 not found")
+   }
 })
 
 server.listen(port,()=>{
