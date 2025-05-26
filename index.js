8c2ddc2f8a1915a72ef71b3aabe2150a2c83fe7d
Neha Bhadauria <146559822+Nehabhadauria19@users.noreply.github.com>
2025-05-26 13:05:48 +0530

    update from 25th may 2025

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello,World!")
+import http from "http"
+const port = 8001
+const server=http.createServer((req,res)=>{
+    res.end("Hello this is my first server and my name is tanmay bhadauria")
+})
+
+server.listen(port,()=>{
+    console.log("server is started")
+})
