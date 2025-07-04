afcbd7a42ae276d3a1a38ef373a1e0420badce59
Tanmay Bhadauria <145855521+Tan28bha@users.noreply.github.com>
2025-07-04 11:35:50 +0530

    Nodejs Completed

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-import http from "http"
-const port = 8001
-const server=http.createServer((req,res)=>{
-   if(req.url == "/"){
-    res.end("Welcome to home route")
-   }
-   else if(req.url == "/about"){
-    res.end("Welcome to about route")
-   }
-   else if(req.url == "/contact"){
-    res.end("Welcome to contact route")
-   }
-   else{
-    res.end("404 not found")
-   }
-})
 
-server.listen(port,()=>{
-    console.log("server is started")
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import http from "http"
+// const port = 8001
+// const server=http.createServer((req,res)=>{
+//    if(req.url == "/"){
+//     res.end("Welcome to home route")
+//    }
+//    else if(req.url == "/about"){
+//     res.end("Welcome to about route")
+//    }
+//    else if(req.url == "/contact"){
+//     res.end("Welcome to contact route")
+//    }
+//    else{
+//     res.end("404 not found")
+//    }
+// })
+
+// server.listen(port,()=>{
+//     console.log("server is started")
+// })
+
